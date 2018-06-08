a084294f6fd2f1cab1a523e0a4f97203f4a71c08
Maxime Desmoulins <desmoulins.maxime.primo@gmail.com>
2018-06-08 11:11:09 +0200

    Update vsh_list.sh

--- vsh_list.sh
+++ vsh_list.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-#Creation de la fonction vsh_list() permettant de lister les archives dans le dossier archives
+# Création de la fonction vsh_list() permettant de lister les archives dans le dossier archives
+#
+# Résultat souhaité : 
+#
+# Il y a actuellement n archives.
+#
+# archive1
+# archive2
+# archive3
+# archive4
+
 
 vsh_list(){
-      
-      ls -t ./archives/
 
+      nbLignes=$(ls -t ./archives/ | wc -l) # retourne le nombre de lignes soit le nombres d'archives sur le serveur.     
+      echo "\nIl y a actuellement $nbLignes archives.\n"
+      ls -1t ./archives/
+      echo ""
+      
 }
