1e7837c2ac9119073e0744fc4f0cf6b4e83d5710
Maxime Desmoulins <desmoulins.maxime.primo@gmail.com>
2018-06-08 12:27:25 +0200

    Update srv.sh

--- srv.sh
+++ srv.sh
@@ -62,7 +62,9 @@ function interaction() {
 
 function commande-vsh_list(){
       nbLignes=$(ls -t ./archives/ | wc -l) # retourne le nombre de lignes soit le nombres d'archives sur le serveur.     
-      echo "\nIl y a actuellement $nbLignes archives.\n"
+      echo ""
+      echo "Il y a actuellement $nbLignes archives sur le serveur"
+      echo ""
       ls -1t ./archives/
       echo ""
 }
