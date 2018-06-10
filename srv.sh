6fa0af71e5d01712b02991c91a6eff828e706e60
Maxime Desmoulins <desmoulins.maxime.primo@gmail.com>
2018-06-10 14:21:12 +0200

    Update srv.sh

--- srv.sh
+++ srv.sh
@@ -69,6 +69,103 @@ function mode-vsh_list(){
       echo ""
 }
 
+function mode-vsh-extract() {
+
+    echo "[Server] You asked for the extraction of the following archive(s): $archive"
+
+
+  ARCHIVE=archives/$archive
+
+  mkdir temporary_files
+  cat $ARCHIVE | grep "directory [A-Za-z0-9]*/" | sed "s/directory //g" > mydirectories.txt
+
+
+  xargs -I {} mkdir -p "{}" < mydirectories.txt
+
+
+
+  while read one_of_the_paths; do
+
+    THE_PATH=$one_of_the_paths
+
+    FILES_AND_DIRS_RIGHTS=$(awk -v THE_PATH=$THE_PATH'$' '$0~THE_PATH{flag=1;next}/@/{flag=0} flag' $ARCHIVE)
+    RIGHTS=$(echo "$FILES_AND_DIRS_RIGHTS" | cut -f2 -d ' ')
+    FILES_AND_DIRS=$(echo "$FILES_AND_DIRS_RIGHTS" | cut -f1 -d ' ')
+
+    echo "$FILES_AND_DIRS_RIGHTS" > temporary_files/FILES_AND_DIRS_RIGHTS.txt
+    echo "$RIGHTS" > temporary_files/RIGHTS.txt
+    echo "$FILES_AND_DIRS" > temporary_files/FILES_AND_DIRS.txt
+
+
+    I=1
+    while read lines; do
+
+    rights=$(cat temporary_files/RIGHTS.txt | sed -n $I'p')
+    files_and_dirs=$(cat temporary_files/FILES_AND_DIRS.txt | sed -n $I'p')
+
+    user_rights_1=$(echo $rights | cut -c2)
+    user_rights_2=$(echo $rights | cut -c3)
+    user_rights_3=$(echo $rights | cut -c4)
+
+    group_rights_1=$(echo $rights | cut -c5)
+    group_rights_2=$(echo $rights | cut -c6)
+    group_rights_3=$(echo $rights | cut -c7)
+
+    other_rights_1=$(echo $rights | cut -c8)
+    other_rights_2=$(echo $rights | cut -c9)
+    other_rights_3=$(echo $rights | cut -c10)
+
+
+
+    if [[ $rights == d* ]]; then
+       echo "------------------"
+       echo "The server found a directory located in $THE_PATH/$files_and_dirs"
+       echo "Adding the rights $rights to it..."
+
+       chmod u+$user_rights_1 $THE_PATH'/'$files_and_dirs
+       chmod u+$user_rights_2 $THE_PATH'/'$files_and_dirs
+       chmod u+$user_rights_3 $THE_PATH'/'$files_and_dirs
+
+       chmod g+$group_rights_1 $THE_PATH'/'$files_and_dirs
+       chmod g+$group_rights_2 $THE_PATH'/'$files_and_dirs
+       chmod g+$group_rights_3 $THE_PATH'/'$files_and_dirs
+
+       chmod o+$other_rights_1 $THE_PATH'/'$files_and_dirs
+       chmod o+$other_rights_2 $THE_PATH'/'$files_and_dirs
+       chmod o+$other_rights_3 $THE_PATH'/'$files_and_dirs
+
+
+    elif [[ $rights  == -* ]]; then
+       echo "------------------"
+       echo "The server found a file located in $THE_PATH/$files_and_dirs"
+       echo "Adding the rights $rights to it..."
+
+       touch $THE_PATH'/'$files_and_dirs
+
+       chmod u+$user_rights_1 $THE_PATH'/'$files_and_dirs
+       chmod u+$user_rights_2 $THE_PATH'/'$files_and_dirs
+       chmod u+$user_rights_3 $THE_PATH'/'$files_and_dirs
+
+       chmod g+$group_rights_1 $THE_PATH'/'$files_and_dirs
+       chmod g+$group_rights_2 $THE_PATH'/'$files_and_dirs
+       chmod g+$group_rights_3 $THE_PATH'/'$files_and_dirs
+
+       chmod o+$other_rights_1 $THE_PATH'/'$files_and_dirs
+       chmod o+$other_rights_2 $THE_PATH'/'$files_and_dirs
+       chmod o+$other_rights_3 $THE_PATH'/'$files_and_dirs
+     fi
+    let "I++"
+
+    done <temporary_files/FILES_AND_DIRS_RIGHTS.txt
+
+
+  done <mydirectories.txt
+
+#Cleaning...
+#rm -f mydirectories.txt
+rm -rf temporary_files/*
+}
+
 function mode-non-compris () {
    echo ""
    echo "Ce mode n'existe pas , vous avez le choix entre :"
